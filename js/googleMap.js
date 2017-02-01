afa87c7466637a0ef5d067cd360931dbcf0d5870
daniiltserin <daniilborowkow@yandex.ru>
2017-02-01 13:12:29 +0600

    Normal page portfolio

--- js/googleMap.js
+++
@@ -1,9 +0,0 @@
-ymaps.ready(init);
-    var myMap;
-
-    function init(){     
-        myMap = new ymaps.Map("ymaps", {
-            center: [43.236893, 76.915401],
-            zoom: 5
-        });
-    }
